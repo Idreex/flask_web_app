75a9bdf809c559c0d48c26496166c12d5f9a4974
Idreex <idreesademola07@gmail.com>
2023-11-13 21:55:04 +0100

    views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
+import os
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html', message='Welcome Home')
+    return render_template('home.html')
 
